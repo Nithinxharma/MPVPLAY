807e8641e50255c52b517a7097dec961a4bb20aa
Nithinxharma <nithinxharma@gmail.com>
2026-06-14 07:29:41 +0530

    Create youtube/model/YoutubeModels.kt

---
+++ app/src/main/java/app/marlboroadvance/mpvex/youtube/model/YoutubeModels.kt
@@ -0,0 +1,42 @@
+package app.marlboroadvance.mpvex.youtube.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class YoutubeVideo(
+    val videoId: String,
+    val title: String,
+    val description: String = "",
+    val viewCount: Long = 0,
+    val publishedText: String = "",
+    val lengthSeconds: Int = 0,
+    val author: String = "",
+    val videoThumbnails: List<YoutubeThumbnail> = emptyList()
+) {
+    fun getBestThumbnailUrl(): String {
+        return videoThumbnails.maxByOrNull { it.width }?.url 
+            ?: "https://img.youtube.com/vi/$videoId/maxresdefault.jpg"
+    }
+}
+
+@Serializable
+data class YoutubeThumbnail(
+    val quality: String = "",
+    val url: String,
+    val width: Int = 0,
+    val height: Int = 0
+)
+
+@Serializable
+data class YoutubeFormat(
+    val url: String,
+    val container: String = "",
+    val qualityLabel: String = "",
+    val type: String = ""
+)
+
+@Serializable
+data class VideoDataResponse(
+    val formatStreams: List<YoutubeFormat> = emptyList(),
+    val adaptiveFormats: List<YoutubeFormat> = emptyList()
+)
